8321066c93541c6607aa02ff7ba081da0ac1790b
Sebastián González <seba.gnh@hgmail.com>
2022-01-04 16:52:18 -0300

    se agregó estructura con titulo e imagenes

--- README.md
+++ README.md
@@ -1,21 +1,27 @@
 Sitio web conectado a la api de https://www.themoviedb.org/ utilizando Async Await
 
+
+
+
+
+
+
+
 https://sebagnh.github.io/Api-Peliculas/
 
 https://github.com/SebaGNH/Api-Peliculas.git
 
 
 
-Clave de la API (v3 auth)
-094387692d60e64212bac949a60c19bc
 
 
 Ejemplo de solicitud de API
 https://api.themoviedb.org/3/movie/550?api_key=094387692d60e64212bac949a60c19bc
 
+Api Películas populares
+https://api.themoviedb.org/3/movie/popular?api_key=094387692d60e64212bac949a60c19bc&languaje=es-AR
 
-Token de acceso de lectura a la API (v4 auth)
-eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwOTQzODc2OTJkNjBlNjQyMTJiYWM5NDlhNjBjMTliYyIsInN1YiI6IjYxZDQ4OWE0NTJkYzdmMDBhNGY2NDllOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ujRLxS_kQ5FrRTO4qx15Cgp6cnUKbiUlEQ4hDsgUYBQ
+
 
 
 
--- js/app.js
+++ js/app.js
@@ -1,15 +1,32 @@
 const cargarPeliculas = async() => { /* async <-- debe estar para utilizar el await */
-
     try{
     const respuesta = 
-    await fetch('https://api.themoviedb.org/3/movie/550?api_key=094387692d60e64212bac949a60c19bc&languaje=es-MX'); /* await <-- espera la conección */
+    await fetch('https://api.themoviedb.org/3/movie/popular?api_key=094387692d60e64212bac949a60c19bc&languaje=es-AR'); /* await <-- espera la conección */
     /*  <-- https://developers.themoviedb.org/3/movies/get-movie-details */
     //console.log(respuesta); /*  <-- Verificamos si se conectó correctamente */
 
     const datos = await respuesta.json();
-    console.log(datos);
-    console.log(datos.original_title);
+    //console.log(datos);
+    /* console.log("Nombre de película: "+datos.title) */
+    
 
+    console.log(datos.results); /*  <-- Resultados peliculas populares */
+
+    let peliculas = "";
+    datos.results.forEach(pelicula => {
+        //console.log(pelicula.title);
+        const imagen = "https://image.tmdb.org/t/p/w500/" + pelicula.poster_path;
+        peliculas += ` 
+        <div class="pelicula">
+            <h2 class="titulo">${pelicula.title}</h2>
+            <img class="poster" src="${imagen}" alt="${pelicula.title}">
+        </div>
+        
+        `;
+        //peliculas = `<p>hola</p>`;
+        document.getElementById("contenedor").innerHTML = peliculas;
+        
+    });
 
 
     }catch(error){ /*  <-- Al trabajar con api es recomendable utilizar try - catch */
